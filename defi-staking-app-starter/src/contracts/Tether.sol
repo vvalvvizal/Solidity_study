c46b1678be6865b78f7f72452a455c931e37e7f0
vvalvvizal <vvalvvizal@gmail.com>
2024-12-02 01:30:41 +0900

    feat : 승인, 전송, 제 3자 전송 기능 구현
    -function transfer
    -function approve
    -function transferFrom

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -7,5 +7,49 @@ contract Tether{
     string symbol='USDT'; //종목코드
     uint256 totalSupply = 1000000000000000000;// 총공급 
     uint decimals = 18;
+    
+    event Transfer(
+        address indexed _from,
+        address indexed _to ,
+        uint _value,
+    )
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address=>uint256) public balanceOf;
+    mapping(address=> mapping(address=>uint256)) public allowance ;
+    constructor(){
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[msg.sender]>=_value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(_value<=balanceOf[_from]);
+        require(_value<=allowance[_from][msg.sender]);
+
+        balanceOf[_to] += _value;
+        balanceOf[_from] -= value;
+
+        allowance[msg.sender][_from] -=value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
+
